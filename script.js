70c21ce4383521ca432f52d0139781183b2bd295
Walker <walker.m.oliver@gmail.com>
2022-07-29 13:44:08 -0400

    Add delete button to each row

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const form = document.querySelector("form");
 let myLibrary = [];
 
 //constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.status = status;
 }
 
 //clear all books from table
@@ -22,27 +22,50 @@ function clearTable() {
   }
 }
 
+//adds a delete button to each table row
+function addDeleteButton(el, index) {
+  const deleteBtn = el.appendChild(document.createElement("button"));
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.innerHTML = `<img class="delete-icon" src="img/trash.png">`;
+
+  deleteBtn.addEventListener("click", function () {
+    const row = document.querySelector(`[data-index="${index}"]`);
+    row.remove();
+    myLibrary.splice(index, 1);
+    displayBooks(myLibrary);
+  });
+}
+
 //display books in table
 function displayBooks(library) {
   clearTable();
 
   library.forEach((book) => {
+    //create table row
     const row = table.insertRow();
     const cell1 = row.insertCell();
     const cell2 = row.insertCell();
     const cell3 = row.insertCell();
     const cell4 = row.insertCell();
 
+    //give each row an index #
+    const index = myLibrary.indexOf(book);
+    row.setAttribute("data-index", index);
+
+    //fill cells with input from form
     cell1.textContent = book.title;
     cell2.textContent = book.author;
     cell3.textContent = book.pages;
-    cell4.textContent = book.read;
+    cell4.textContent = book.status;
+
+    //add delete button to each row
+    addDeleteButton(cell4, index);
   });
 }
 
-//Add book to library
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
+//Add new book
+function addBookToLibrary(title, author, pages, status) {
+  const newBook = new Book(title, author, pages, status);
   myLibrary.push(newBook);
   displayBooks(myLibrary);
 }
@@ -58,12 +81,30 @@ submitBtn.addEventListener("click", function () {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").value;
+  const status = document.getElementById("status").value;
 
-  addBookToLibrary(title, author, pages, read);
+  addBookToLibrary(title, author, pages, status);
 
   form.reset();
   form.classList.add("hidden");
   newBookBtn.classList.remove("hidden");
   submitBtn.classList.add("hidden");
 });
+
+//TO DO
+//6) //add drop down for read/unread.
+//save answer in mylibrary in submitBtn function
+//display answer as text in table
+//add toggle switch to cell
+//connect switch to form input (separate funciton called from submit btn?)
+//when switch is clicked, edit book info in mylibrary
+
+//add keyboard listener to submit button
+//style form
+//make all form inputs required to submit
+//fix table width (https://stackoverflow.com/questions/4457506/set-the-table-column-width-constant-regardless-of-the-amount-of-text-in-its-cell)
+
+addBookToLibrary("Book 1", "Author Name", 100, "Read");
+addBookToLibrary("Book 2", "Author Name", 100, "Read");
+addBookToLibrary("Book 3", "Author Name", 100, "Read");
+addBookToLibrary("Book 4", "Author Name", 100, "Read");
